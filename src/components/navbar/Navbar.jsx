7c766477f941032ed962b83b5d38c60977cd7010
FelipeDiasR <felipediasf0105@gmail.com>
2024-05-31 18:04:37 -0300

    refactor2

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 import Logotipo from '../../img/logotipo.svg';
-import Buybutton from '../../img/Buy.png';
 import './navbar.css';
 
 const Menu = () => (
   <>
-   <button><a href="#lauchpad">Preesale</a></button>
+  <a href="https://meowl-1.gitbook.io/meowlverse-whitepaper/" target="_blank" rel="noopener noreferrer">
+         Whitepaper</a>
+  
   </>
 );
 
@@ -16,43 +17,35 @@ const Navbar = () => {
 
   return (
     <div className="meow__navbar">
-      <div className="meow__navbar-links">
-        <div className="meow__navbar-links_logo">
-          <img className="meow__navbar-links_logo_img" src={Logotipo} alt="logo" />
+      <div className='meow__navbar_logo'>
+        <img src={Logotipo} alt='logotipo'/> 
         </div>
-        <div className='meow__navbar-links_container'>
-          <Menu />
+      <div className='meow__navbar_preesale'>
+        <button > Preesale </button>    
         </div>
-      </div>
-
-      <div className='meow__navbar-whitepaper-buy'>
-        <p><a href="https://meowl-1.gitbook.io/meowlverse-whitepaper/" target="_blank" rel="noopener noreferrer">Whitepaper</a></p>
-      </div>
-
-      <div className='meow__navbar-menu_container_button'>
-        <img className="meow__navbar-menu_container_img" src={Buybutton} alt="buybutton" />
-      </div>
-
+      <div className='meow__navbar_whitepaper_wallet'>
+        <a href="https://meowl-1.gitbook.io/meowlverse-whitepaper/" target="_blank" rel="noopener noreferrer">
+         Whitepaper</a>
+        <button> Connect Wallet</button>
+        </div>
       <div className='meow__navbar-menu'>
         {toggleMenu
-          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
-          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
-        }
-
-        {toggleMenu && (
-          <div className="meow__navbar-menu_container">
-            <div className="meow__navbar-menu_container_link">
-              <Menu />
-              <div className='meow__navbar_menu_whitepaper-buy'>
-                <p><a href="https://meowl-1.gitbook.io/meowlverse-whitepaper/" target="_blank" rel="noopener noreferrer">Whitepaper</a></p>
-              </div>
-              <div className='meow__navbar-menu_container_button_menu'>
-                <p><a href="#lauchpad">Buy</a></p>
-              </div>
+         ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+         : < RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+         }
+         {toggleMenu && 
+           <div className='meow__navbar-menu_container scale-up-center'> 
+            <div className='meow__navar-menu_container-links'> 
+            <Menu/>
             </div>
-          </div>
-        )}
-      </div>
+           </div>
+           
+           
+           }
+        </div>
+
+
+      
     </div>
   );
 }
