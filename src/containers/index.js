83fd02163f3e2f5a2478c23f6af1a9be0b1a0c73
FelipeDiasR <felipediasf0105@gmail.com>
2024-06-04 11:50:50 -0300

    new page

--- src/containers/index.js
+++ src/containers/index.js
@@ -4,3 +4,11 @@ export { default as Preesale } from './pages/home/preesale/Preesale.jsx';
 export { default as Tokenomics } from './pages/home/tokenomics/Tokenomics.jsx';
 export { default as Brand } from './pages/home/brand/Brand.jsx';
 export { default as Cta } from './pages/home/cta/Cta.jsx';
+
+
+
+//Lauchpad
+
+export { default as Alert } from './pages/lauchpad/alert/Alert.jsx';
+export { default as Banner } from './pages/lauchpad/banner/Banner.jsx';
+export { default as Cards } from './pages/lauchpad/projects/Cards.jsx';
--- src/pages/lauchpad/Lauchpad.jsx
+++ src/pages/lauchpad/Lauchpad.jsx
@@ -1,27 +1,72 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-
+import './lauchpad.css';
 //importing parts of the page
 //import { /*Article,*Fature,*/ Navbar } from '../../components';
-import { Partners, Header, Preesale, Tokenomics, Brand, Cta } from '../../containers';
+import { Alert, Cards, Banner, /*Tokenomics, Brand, Cta*/ } from '../../containers';
   
-
+import { format } from 'date-fns';
 
 
 const Launchpad = () => {
-  return (
-    <div className='meow_home'>
-      <div className='gradient_bg'  >
-         <Header />
-        </div>
-      
-      <Cta/>
-      
-      
-     
-    </div>
+  const [repositories , setRepositories] = useState([]);
 
+  useEffect ( ( ) =>  {
+    const searchingRepositories = async () => {
+      try {
+        const response = await fetch('/cards.json');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log('Data fetched:', data);
+        setRepositories(data.projects); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    searchingRepositories();
+  }, []);
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return format(date, 'MMMM dd, yyyy');
+  };
+
+  const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(amount);
+  };
+
+
+  return (
+      <div className='meow__lauchpad'>
+        <Alert /> 
+        <h1 className='meow__lauchpad_global'> Welcome to MeowPad</h1>
+         {
+           repositories.length > 0 ? (
+            <div className='meow__lauchpad_cards'>
+            {console.log(repositories)}
+            { 
+              repositories.map((repo) => (
+                <Cards logo={repo.image} status={repo.status} name={repo.name}
+                website={repo.social_links.website} twitter={repo.social_links.twitter}
+                telegram={repo.social_links.telegram} open_sale={formatDate(repo.sales_opening_date)}
+                close_sale={formatDate(repo.sales_closing_date)} token_price={repo.token_price}
+                total_raise={formatCurrency(repo.total_to_raise)}/>
+              ) )          
+            }
+         </div>
+
+           ) : (
+             <p> Soon we are going to have projects...</p>
+           )
+         }
+        <Banner/>
+      </div>
 
   )
 
