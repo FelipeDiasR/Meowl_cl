944d02df42b08ad7fd4676c46d3e77fd7d566ce5
FelipeDiasR <felipediasf0105@gmail.com>
2024-07-01 23:59:56 -0300

    gaming

--- src/containers/pages/gamming/cards_games/CardsGames.jsx
+++ src/containers/pages/gamming/cards_games/CardsGames.jsx
@@ -1,22 +1,34 @@
 // src/components/LoadingPopup.jsx
 import React from 'react';
 import './cards_games.css'; // Arquivo CSS para estilização
+//import background from '../../../../img/gamecards/meowlrouletback.svg'
+//import wheelLogo from '../../../../img/gamecards/meowlwheel.svg';
+
+const CardsGames = ({ name, status, description, open_toplay, image1,
+  image2}) => {
 
 
 
-
-function CardsGames () {
 
   return (
     <div className="meow__cardsGames">
-        <div className="meow__cardsGames__container">
-            <div className="meow__cardsGames_content">
-                <h1> Gamming platform</h1>
-                <p> Soon we will find here all the main games from MewolVerse</p>
-             </div>
-             <div className="meow__cardsGames_welcome">
-                <p>Welcome to the MeowlGaming platform! Here you can choose a game, place your bets, and have fun!</p>
-             </div>
+      <div className="meow__cardsGames__container">
+        <div className="meow__cardsGames__background">
+          <img src={image1} alt='projectbackground' className="background-image"/> 
+          <button className="status-button">
+            {status}
+          </button>
+        </div>
+        <div className="meow__cardsGames__logo">
+          <img src={image2} alt='projectlogo'/>
+        </div>
+        <div className="meow__cardsGames__text">
+          <h1> {name} </h1>
+          <p>{description}</p>
+          <button className="play-button">
+            {status}
+          </button>
+        </div>
       </div>
     </div>
   );
