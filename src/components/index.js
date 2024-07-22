7f649ee69fa10c837de1fb131393f4b437368283
FelipeDiasR <felipediasf0105@gmail.com>
2024-07-21 23:18:31 -0300

    testing time!

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,10 @@ export { default as Footer } from '../components/footer/Footer.jsx';
 export { default as Loading } from './loading/Loading.jsx';
 export { default as Approved } from './approved/Approved.jsx';
 export { default as Denied } from './denied/Denied.jsx';
+export { default as GeneralPopup } from './generalpopup/GeneralPopup.jsx';
 
 
 export { default as ScrollTop } from './scrollTop/ScrollTop.jsx';
 export { default as InfoPopup } from './info/InfoPopup.jsx';
 export { default as StartingWheel } from './wheel/starter/StartingWheel.jsx';
+
